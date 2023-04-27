f4c2a4652c7c786469a7017b66db8e2013804563
Sachin pandit <127498624+Sachin-Pandit-Shankar@users.noreply.github.com>
2023-04-27 12:08:06 +0530

    Update index.js

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,11 +1,9 @@
 // Write your code here
-import {Component} from 'react'
 import './index.css'
 
-class Message extends Component {
-  render() {
-    return <h1 className="heading">Welcome User</h1>
-  }
-}
+const Message = props => {
+  const {content} = props
 
+  return <h1 className="heading">{content}</h1>
+}
 export default Message
