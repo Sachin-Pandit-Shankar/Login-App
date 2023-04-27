0f45254918ea192913c084b291c50fc8eeed638f
Sachin pandit <127498624+Sachin-Pandit-Shankar@users.noreply.github.com>
2023-04-27 12:07:39 +0530

    Update index.js

--- src/components/Logout/index.js
+++ src/components/Logout/index.js
@@ -1,14 +1,13 @@
 // Write your code here
-import {Component} from 'react'
 import './index.css'
 
-class Logout extends Component {
-  render() {
-    return (
-      <button type="button" className="button">
-        Logout
-      </button>
-    )
-  }
+const Logout = props => {
+  const {changeStatus} = props
+
+  return (
+    <button type="button" className="button" onClick={changeStatus}>
+      Login
+    </button>
+  )
 }
 export default Logout
